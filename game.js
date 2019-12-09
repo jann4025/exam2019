06e6c31039a3f99380be4558621908fe384f18b1
Jannick Holm <jannick@wecode.dk>
2019-12-09 14:53:26 +0100

    Game working with game win screen - Desktop

--- game.js
+++ game.js
@@ -1,27 +1,50 @@
-'use strict'
+'use strict';
+
 document.addEventListener('DOMContentLoaded', start);
+
 let newDeck;
 
+let { dealer, player, playerSum, dealerSum } = setInitialValues();
+
 let deck = document.querySelector("#deck");
-
-let player = [];
-
-let playerValue = 0;
-
-let dealer = [];
-
-let dealerValue = 0;
 
 let temp = document.querySelector("template");
 
 let hitButton = document.querySelector(".btn.hit");
 
 const btns = document.querySelector("#btns");
+
+
+
+
+
+function newFunction() {
+    let newDeck;
+    return newDeck;
+}
+
 function start() {
     console.log('Connection between the DOM and the Script was successfull! Nice 😎');
     creatNewDeck();
     addEventListener();
+
+
 }
+
+function reset() {
+    let newDeck;
+    let { dealer, player, playerSum, dealerSum } = setInitialValues();
+    btns.style.display = "block";
+}
+
+function setInitialValues() {
+    let player = [];
+    let playerSum = 0;
+    let dealer = [];
+    let dealerSum = 0;
+    return { dealer, player, playerSum, dealerSum };
+}
+
 
 function creatNewDeck() {
     newDeck = generateDeck();
@@ -33,9 +56,7 @@ function creatNewDeck() {
 }
 
 function generateDeck() {
-    let suits = ["spades", "diamonds", "clubs", "hearts"];
-    let values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-    let deck = new Array();
+    let { suits, values, deck } = cardInfo();
 
     for (let i = 0; i < suits.length; i++) {
         for (let x = 0; x < values.length; x++) {
@@ -49,6 +70,13 @@ function generateDeck() {
         }
     }
     return deck;
+}
+
+function cardInfo() {
+    let suits = ["spades", "diamonds", "clubs", "hearts"];
+    let values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+    let deck = new Array();
+    return { suits, values, deck };
 }
 
 function addEventListener() {
@@ -73,6 +101,7 @@ function firstDeal() {
     }
 
 
+
     //player
     for (let x = 0; x < 2; x++) {
         let rN = Math.floor(Math.random() * newDeck.length);
@@ -85,39 +114,50 @@ function firstDeal() {
         newDeck.splice(rN, 1);
         player.push(card);
     }
-    console.log(player)
-    console.log(dealer)
-    checkValue();
+    console.log(player);
+    console.log(dealer);
     player.forEach(x => flipPlayer(x.ID));
     dealer.forEach(x => flipDealer(x.ID));
+    checkValue();
+}
+
+function checkWinner() {
+    if (playerSum == 21 && player[0].Value == 11) {
+        winScreen();
+
+    } else if (playerSum == 21 && player[1].Value == 11) {
+        winScreen();
+
+    } else if (playerSum > 21) {
+        alert("Dealer Wins");
+        reset();
+    } else if (dealerSum > 21) {
+        winScreen();
+
+    } else if (dealerSum <= 21 && dealerSum > playerSum) {
+        alert("Dealer Wins");
+        reset();
+    } else if (playerSum <= 21 && playerSum > dealerSum) {
+        winScreen();
+
+    } else if (playerSum == dealerSum) {
+        alert("Tie");
+        reset();
+    }
+
 }
 
 function flipPlayer(id) {
     btns.style.display = "none";
-    // Select cards and dist container
     const dist = document.querySelector("#player-container");
     let split = dist.childNodes;
     let el = document.querySelector(`.${id}`);
-
-    //FLIP start
-
-    //Select First posistion
     const first = el.getBoundingClientRect();
-
-    //Append to the distination container
     dist.appendChild(el);
-
-    //Where is the cards know? 
     const last = el.getBoundingClientRect();
+    const { invertX, invertY } = getInvert(first, last);
 
-    //Find the diference in px between the first and last position
-    const invertY = first.top - last.top;
-    const invertX = first.left - last.left;
-
-    //Move the cards back to the first position with transform
     el.style.transform = `translate(${invertX}px, ${invertY}px)`;
-
-    //Start animation on the first card in the player-cards container
     let animation = split[3].animate([
         { transform: `translate(${invertX}px, ${invertY}px)` },
         { transform: 'translate(0,0)' }
@@ -129,7 +169,7 @@ function flipPlayer(id) {
     animation.addEventListener('finish', () => {
         //When the animation ends add a css keyframe that flips the card
         split[3].classList.add("flip");
-        document.querySelector(".flip").addEventListener('transitionend', () => {
+        split[3].addEventListener('transitionend', () => {
 
             //When the flip animation ends, start animation on the second card in the player-cards container
             let animation2 = dist.childNodes[4].animate([
@@ -147,11 +187,20 @@ function flipPlayer(id) {
 
                 split[4].addEventListener('transitionend', () => {
                     btns.style.display = "block";
-                    showPlayerValues();
+                    showplayerSum();
+                    if (playerSum == 21 && player[0].Value == 10 && player[1].Value == 11 || player[1].Value == 10 && player[0].Value == 11) {
+                        winScreen();
+                    }
                 });
             });
         });
     });
+}
+
+function getInvert(first, last) {
+    const invertY = first.top - last.top;
+    const invertX = first.left - last.left;
+    return { invertX, invertY };
 }
 
 function flipDealer(id) {
@@ -162,7 +211,6 @@ function flipDealer(id) {
     const last = el.getBoundingClientRect();
     const invertY = first.top - last.top;
     const invertX = first.left - last.left;
-    console.log(first);
     el.style.transform = `translate(${invertX}px, ${invertY}px)`;
     let animation = dist.childNodes[3].animate([
         { transform: `translate(${invertX}px, ${invertY}px)` },
@@ -174,8 +222,8 @@ function flipDealer(id) {
     });
     animation.addEventListener('finish', () => {
         dist.childNodes[3].classList.add("flip");
-        document.querySelector(".flip").addEventListener('transitionend', () => {
-            showDealerValue();
+        dist.childNodes[3].addEventListener('transitionend', () => {
+            showdealerSum();
             let animation2 = dist.childNodes[4].animate([
                 { transform: `translate(${invertX}px, ${invertY}px)` },
                 { transform: 'translate(0,0)' }
@@ -186,7 +234,6 @@ function flipDealer(id) {
             });
 
             animation2.addEventListener('finish', () => {
-                // setTimeout(showDealerValue, 400);
             });
         });
     });
@@ -202,8 +249,8 @@ function dealNewCardPlayer() {
     klon.querySelector(".back_image").src = newDeck[rN].Image;
     klon.querySelector(".card").classList.add(id);
     deck.appendChild(klon);
-    newDeck.splice(rN, 1);
     checkValue();
+    newDeck.splice(rN, 1);
     animateCard(id);
 
     function animateCard(id) {
@@ -227,13 +274,22 @@ function dealNewCardPlayer() {
         animation.addEventListener('finish', () => {
             el.classList.add("flip");
             btns.style.display = "block";
-            showPlayerValues();
+            el.addEventListener("animationend", () => {
+                showplayerSum();
+                if (playerSum > 21) {
+                    showplayerSum();
+                    setTimeout(() => {
+                        alert("Dealer wins");
+                    }, 200);
+
+                }
+            });
         });
     }
 }
 
 function dealNewCardDealer() {
-    if (dealerValue < 17) {
+    if (dealerSum < 17) {
         let rN = Math.floor(Math.random() * newDeck.length);
         const id = newDeck[rN].Suit.charAt(0).toUpperCase() + newDeck[rN].Value;
         let card = { Value: newDeck[rN].Value, Suit: newDeck[rN].Suit, Image: newDeck[rN].Image, ID: id };
@@ -243,82 +299,102 @@ function dealNewCardDealer() {
         klon.querySelector(".card").classList.add(id);
         deck.appendChild(klon);
         newDeck.splice(rN, 1);
-        animateCard(id);
         checkValue();
-        function animateCard(id) {
-            let el = document.querySelector(`.${id}`);
-            let dist = document.querySelector(`#dealer-container`);
-            const first = el.getBoundingClientRect();
-            dist.appendChild(el);
-            const last = el.getBoundingClientRect();
-            const invertY = first.top - last.top;
-            const invertX = first.left - last.left;
-            console.log(first);
-            el.style.transform = `translate(${invertX}px, ${invertY}px)`;
-            let animation = el.animate([
-                { transform: `translate(${invertX}px, ${invertY}px)` },
-                { transform: 'translate(0,0)' }
-            ], {
-                duration: 800,
-                easing: 'cubic-bezier(0,0,0.32,1)',
-                fill: "forwards"
+        animateCard(id);
+    }
+    function animateCard(id) {
+        let el = document.querySelector(`.${id}`);
+        let dist = document.querySelector(`#dealer-container`);
+        const first = el.getBoundingClientRect();
+        dist.appendChild(el);
+        const last = el.getBoundingClientRect();
+        const invertY = first.top - last.top;
+        const invertX = first.left - last.left;
+        el.style.transform = `translate(${invertX}px, ${invertY}px)`;
+        let animation = el.animate([
+            { transform: `translate(${invertX}px, ${invertY}px)` },
+            { transform: 'translate(0,0)' }
+        ], {
+            duration: 800,
+            easing: 'cubic-bezier(0,0,0.32,1)',
+            fill: "forwards"
+        });
+        animation.addEventListener('finish', e => {
+            el.classList.add("flip");
+            el.addEventListener('animationend', e => {
+                showdealerSumFull();
+                if (dealerSum < 17) {
+                    dealNewCardDealer();
+                } else {
+                    showdealerSumFull();
+                    setTimeout(checkWinner, 200);
+                }
+
             });
-            animation.addEventListener('finish', () => {
-                el.classList.add("flip");
-            });
-        }
+        });
     }
 }
 
 function checkValue() {
-    dealerValue = 0;
-    playerValue = 0;
-    //dealer
-    for (let x = 0; x < dealer.length; x++) {
-
-        if (dealer[x].Value == "J" || dealer[x].Value == "K" || dealer[x].Value == "Q") {
-            dealer[x].Value = 10;
-        } else if (dealer[x].Value == "A") {
-            dealer[x].Value = 11;
+    dealerSum = 0;
+    playerSum = 0;
+    for (let i = 0; i < dealer.length; i++) {
+        if (dealer[i].Value == "J" || dealer[i].Value == "K" || dealer[i].Value == "Q") {
+            dealer[i].Value = 10;
+        } else if (dealer[i].Value == "A") {
+            dealer[i].Value = 11;
         }
-        dealerValue = dealerValue + dealer[x].Value;
+        dealerSum = dealerSum + dealer[i].Value;
     }
     //player
-    for (let x = 0; x < player.length; x++) {
-        if (player[x].Value == "J" || player[x].Value == "K" || player[x].Value == "Q") {
-            player[x].Value = 10;
-        } else if (player[x].Value == "A") {
-            player[x].Value = 11;
+    for (let i = 0; i < player.length; i++) {
+        if (player[i].Value == "J" || player[i].Value == "K" || player[i].Value == "Q") {
+            player[i].Value = 10;
+        } else if (player[i].Value == "A") {
+            player[i].Value = 11;
         }
-        playerValue = playerValue + player[x].Value;
+        playerSum = playerSum + player[i].Value;
     }
-
     hitButton.addEventListener("click", dealNewCardPlayer);
     document.querySelector(".stand").addEventListener("click", stand);
-    console.log(dealerValue);
 }
 
 function stand() {
     btns.style.display = "none";
-    document.querySelector("#dealer-container").childNodes[4].classList.add('flip');
-    document.querySelector("#dealer-container").childNodes[4].addEventListener("transitionend", () => {
-        checkValue();
-        if (dealerValue < 17) {
-            dealNewCardDealer();
-        } else {
-            showDealerValuesFull();
-        }
-    });
+    let el = document.querySelector("#dealer-container").lastElementChild;
+    el.classList.add('flip');
+    el.addEventListener("animationend", checkdealerSum);
 }
 
-function showPlayerValues() {
-    document.querySelector("#player-value").innerHTML = playerValue;
+function checkdealerSum() {
+    if (dealerSum < 17) {
+        console.log("foo");
+        showdealerSumFull();
+        dealNewCardDealer();
+    } else {
+        showdealerSumFull();
+        setTimeout(checkWinner, 200);
+    }
 }
 
-function showDealerValue() {
+function showplayerSum() {
+    document.querySelector("#player-value").innerHTML = playerSum;
+}
+
+function showdealerSum() {
     document.querySelector("#dealer-value").innerHTML = dealer[0].Value;
 }
 
-function showDealerValuesFull() {
-    document.querySelector("#dealer-value").innerHTML = dealerValue;
+function showdealerSumFull() {
+    document.querySelector("#dealer-value").innerHTML = dealerSum;
 }
+
+function winScreen() {
+    document.querySelector(".gameEnd").classList.add("fade-in");
+    document.querySelector(".fade-in").addEventListener("animationend", () => {
+        document.querySelector("#confettiAnimation").play();
+    });
+
+
+}
+
