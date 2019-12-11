ad299643e567b1fc1a1481220f11a993d1b1495f
Sarah Davidsen <sarahdavidsen@Sarahs-MacBook-Pro-2.local>
2019-12-11 19:51:32 +0100

    Adding the random package.json I was asked to

---
+++ admin/build/precache-manifest.81dcd11db402c92ff922fc9f06270430.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f7d1d3b53d3c10ca68dc0079765496",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d40997e4145766206207",
+    "url": "/static/css/main.08294c07.chunk.css"
+  },
+  {
+    "revision": "1916e694b3a883efc39b",
+    "url": "/static/js/2.24834729.chunk.js"
+  },
+  {
+    "revision": "d40997e4145766206207",
+    "url": "/static/js/main.fef465dd.chunk.js"
+  },
+  {
+    "revision": "1e4f1b1a214af12f565e",
+    "url": "/static/js/runtime-main.633607c8.js"
+  }
+]);
